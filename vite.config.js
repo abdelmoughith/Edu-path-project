1c8cdbd9433176f744d0f61849cf21e65509f73b
MOUGHIT <abdelmoughith2002@gmail.com>
2025-12-27 01:32:34 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/users': { target: 'http://api-gateway:8089', rewrite: path => path.replace(/^\/api\/users/, '/users') },
-      '/api/courses': { target: 'http://api-gateway:8089', rewrite: path => path.replace(/^\/api\/courses/, '/courses') },
-      '/api/activities': { target: 'http://api-gateway:8089', rewrite: path => path.replace(/^\/api/activities/, '/activities') },
-      '/api/ai': { target: 'http://edupath-ml:8000', rewrite: path => path.replace(/^\/api\/ai/, ''), changeOrigin: true },
-    },
-  },
+      '/api/users': {
+        target: 'http://api-gateway:8089',
+        rewrite: path => path.replace(/^\/api\/users/, '/users')
+      },
+      '/api/courses': {
+        target: 'http://api-gateway:8089',
+        rewrite: path => path.replace(/^\/api\/courses/, '/courses')
+      },
+      '/api/activities': {
+        target: 'http://api-gateway:8089',
+        rewrite: path => path.replace(/^\/api\/activities/, '/activities')
+      },
+      '/api/ai': {
+        target: 'http://edupath-ml:8000',
+        rewrite: path => path.replace(/^\/api\/ai/, ''),
+        changeOrigin: true
+      }
+    }
+  }
 })
